23cb6e16c3429bb98f6a581fbcbb31b0e2b625e7
thienan <thienan2195@gmail.com>
2019-01-10 23:36:20 +0700

    change schema

--- NodeJS/models/gift.js
+++ NodeJS/models/gift.js
@@ -2,6 +2,7 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var GiftSchema = new Schema({
+    userId: {type: String, required: true},
     title: {type: String, required: true},
     imagePathList: {type: Array, required: true},
     description: {type: String, required: true},
