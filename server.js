751929e8aeb117378bcaebac01ecb1c495a59268
thienan <thienan2195@gmail.com>
2019-01-26 23:15:44 +0700

    heroku

--- server.js
+++ server.js
@@ -25,18 +25,6 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(flash());
 app.use(passport.initialize());
 app.use(passport.session());
-app.use(function(req, res, next) {
-  if (req.headers && req.headers.authorization && req.headers.authorization.split(' ')[0] === 'JWT') {
-    jsonwebtoken.verify(req.headers.authorization.split(' ')[1], process.env.JWT_PRIVATE_KEY, function(err, decode) {
-      if (err) req.user = undefined;
-      req.user = decode;
-      next();
-    });
-  } else {
-    req.user = undefined;
-    next();
-  }
-});
 app.listen(PORT, () => console.log('Server started at port in env'));
 app.use('/api', require('./app/api/api'));
 app.use(session({
