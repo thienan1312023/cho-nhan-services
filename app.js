cebda79ec9427a96e21c1a45ea5ab4e41e806b2b
thienan <thienan2195@gmail.com>
2019-01-26 08:59:15 +0700

    up files

---
+++ app.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const mongoose = require('mongoose');
+expressValidator = require('express-validator');
+const bodyParser = require('body-parser');
+const flash = require('connect-flash');
+const session = require('express-session');
+const passport = require('passport');
+const PORT = process.env.PORT || 3004;
+const validatorOptions = {
+
+};
+//const { mongoose } = require('./db.js');
+mongoose.connect('mongodb://chonhancontributor:chonhan11111@ds251210.mlab.com:51210/chonhandb', { useNewUrlParser: true }, function(error){
+  if(!error){
+    console.log("Connect Server mLab chonhanDB Successfully");
+  }else{
+    console.log("Has error when connect");
+  }
+}); 
+var app = express();
+var MongoStore = require('connect-mongo')(session);
+app.use(expressValidator(validatorOptions));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(flash());
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(function(req, res, next) {
+  if (req.headers && req.headers.authorization && req.headers.authorization.split(' ')[0] === 'JWT') {
+    jsonwebtoken.verify(req.headers.authorization.split(' ')[1], process.env.JWT_PRIVATE_KEY, function(err, decode) {
+      if (err) req.user = undefined;
+      req.user = decode;
+      next();
+    });
+  } else {
+    req.user = undefined;
+    next();
+  }
+});
+app.listen(PORT, () => console.log('Server started at port in env'));
+app.use('/api', require('./app/api/api'));
+app.use(session({
+    secret: 'mysupersecret', 
+    resave: false, 
+    saveUninitialized: false,
+    store: new MongoStore({ mongooseConnection: mongoose.connection }),
+    cookie: { maxAge: 180 * 60 * 1000 }
+  }));
+
+
