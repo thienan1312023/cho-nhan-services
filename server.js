cf2ef5d3da1eab41c6a2082ba8f38559381415df
thienan <thienan2195@gmail.com>
2019-01-28 20:10:56 +0700

    Change position cor origin

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser');
 const flash = require('connect-flash');
 const session = require('express-session');
 const passport = require('passport');
-const PORT = process.env.PORT || 3004;
+const PORT = process.env.PORT || 3001;
 const cors = require('cors');
 const validatorOptions = {
 
@@ -20,6 +20,7 @@ mongoose.connect('mongodb://chonhancontributor:chonhan11111@ds251210.mlab.com:51
 }); 
 var app = express();
 var MongoStore = require('connect-mongo')(session);
+app.use(cors({ origin: '*' }));
 app.use(expressValidator(validatorOptions));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
@@ -28,7 +29,6 @@ app.use(passport.initialize());
 app.use(passport.session());
 app.listen(PORT, () => console.log('Server started at port in env'));
 app.use('/api', require('./app/api/api'));
-app.use(cors({ origin: '*' }));
 app.use(session({
     secret: 'mysupersecret', 
     resave: false, 
