cf2580cf57b4766871f68eb0912a1bca169c5c41
thienan <thienan2195@gmail.com>
2019-02-11 20:56:47 +0700

    update login catch err

--- app/api/user/user.js
+++ app/api/user/user.js
@@ -8,70 +8,74 @@ const signupValidator = require('../../shared/validations/signup');
 const router = express.Router();
 
 router.post('/signup', (req, res, next) => {
-  const {isValid, errors } = signupValidator(req.body);
+  const { isValid, errors } = signupValidator(req.body);
   if (!isValid) {
     return res.status(400).json({ error: true, errors });
   }
-    var userNew = new User({
-      lastName: req.body.lastName,
-      firstName: req.body.firstName,
-      password: req.body.password,
-      contactIdentity: req.body.contactIdentity,
-      favoritePosts: req.body.favoritePosts
-    });
-    var salt = bcrypt.genSaltSync(10);
-    bcrypt.hash(userNew.password, salt, function(err, hash) {
-          if(err) return next(err);
-          userNew.password = hash;
-          userNew
-              .save()
-              .then(result => {
-                console.log(result);
-                res.status(201).json({
-                  message: "User created"
-                });
-              })
-              .catch(err => {
-                if(err.code === 11000){
-                  res.status(409);
-                }else{
-                  res.status(500);
-                }
-                next(err);
-              });
+  var userNew = new User({
+    lastName: req.body.lastName,
+    firstName: req.body.firstName,
+    password: req.body.password,
+    contactIdentity: req.body.contactIdentity,
+    favoritePosts: req.body.favoritePosts
+  });
+  var salt = bcrypt.genSaltSync(10);
+  bcrypt.hash(userNew.password, salt, function (err, hash) {
+    if (err) return next(err);
+    userNew.password = hash;
+    userNew
+      .save()
+      .then(result => {
+        console.log(result);
+        res.status(201).json({
+          message: "User created"
+        });
+      })
+      .catch(err => {
+        if (err.code === 11000) {
+          res.status(409);
+        } else {
+          res.status(500);
+        }
+        next(err);
       });
+  });
 });
-    
+
 
 
 router.post('/login', async (req, res, next) => {
   let user;
   try {
     user = await User.findOne({ contactIdentity: req.body.contactIdentity });
-    if(user){
+    if (user) {
       let comparePassword = await bcrypt.compare(req.body.password, user.password);
-      if(comparePassword){
-      const token = await jwt.sign(
-        {
-          contactIdentity: user.contactIdentity,
-          userId: user._id,
-          fullName:  user.lastName + " "+ user.firstName,
-          profileImagePath:  user.profileImagePath
-        },
-        process.env.JWT_SECRET_KEY,
-        {
+      if (comparePassword) {
+        const token = await jwt.sign(
+          {
+            contactIdentity: user.contactIdentity,
+            userId: user._id,
+            fullName: user.lastName + " " + user.firstName,
+            profileImagePath: user.profileImagePath
+          },
+          process.env.JWT_SECRET_KEY,
+          {
             expiresIn: "30d"
-        }
-      );
-      return res.status(200).json({
-        message: "Auth successful",
-        token: token
-      });
-    }
-    }else{
+          }
+        );
+        return res.status(200).json({
+          message: "Auth successful",
+          token: token
+        });
+      } else {
+        res.status(401).json({
+          message: "Login Failed"
+        });
+      }
+    } else {
       res.status(401).json({
         message: "Login Failed"
-      });  
+      });
     }
   } catch (err) {
     res.status(401).json({
